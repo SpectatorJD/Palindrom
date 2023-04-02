9f83050692a498060bfe25ce5d8bd484eafdf413
Tkakishak <tkakishakd@mail.ru>
2023-04-02 17:41:14 +0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(getPalindorm ("Топтать"));
 
     }
+      public static boolean getPalindorm (String word){
+        String w1 = word.toUpperCase();
+        StringBuffer w2=new StringBuffer(w1).reverse();
+        boolean check = true;
+          for (int i = 0; i <w1.length() ; i++) {
+              if (w1.charAt(i)!= w2.charAt(i)){
+                  check=false;
+                  break;
+              }
+          }
+        return check;
+      }
 }
